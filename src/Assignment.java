fa2bd64e2c6d2d80e1a4d27d3f1dd2cf7aa4535d
AlexN572 <anewlands572@gmail.com>
2026-04-13 17:57:48 +0000

    1st commit

---
+++ src/Assignment.java
@@ -0,0 +1,67 @@
+public class Assignment
+{
+
+    private String name;
+    private int dayDue;
+    private int monthDue;
+    private int importanceLevel;
+    private int assignmentLength;
+
+    public Assignment(String name, int dayDue, int monthDue, int importanceLevel, int assignmentLength)
+    {
+        this.name = name;
+        this.dayDue = dayDue;
+        this.monthDue = monthDue;
+        this.importanceLevel = importanceLevel;
+        this.assignmentLength = assignmentLength;
+    }
+
+    //Getters
+    public String getName()
+    {
+        return name;
+    }
+    public int getDayDue()
+    {
+        return dayDue;
+    }
+    public int getMonthDue()
+    {
+        return monthDue;
+    }
+    public int getImportanceLevel()
+    {
+        return importanceLevel;
+    }
+    public int getAssignmentLength()
+    {
+        return assignmentLength;
+    }
+
+    //Setters
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+    public void setDayDue(int dayDue)
+    {
+        this.dayDue = dayDue;
+    }
+    public void setMonthDue(int monthDue)
+    {
+        this.monthDue = monthDue;
+    }
+    public void setImportanceLevel(int importanceLevel)
+    {
+        this.importanceLevel = importanceLevel;
+    }
+    public void setAssignmentLength(int assignmentLength)
+    {
+        this.assignmentLength = assignmentLength;
+    }
+
+    public String toString()
+    {
+        return "Assignment Name: " + name + "\nDue Date: " + monthDue + "/" + dayDue + "\nImportance Level: " + importanceLevel + "\nTime to complete: " + assignmentLength;
+    }
+}
---
+++ src/Day.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+public class Day
+{
+    private String name;
+    private ArrayList<Assignment> assignmentList;
+
+    public Day(String name)
+    {
+        this.name = name;
+        assignmentList = new ArrayList<Assignment>();
+    }
+
+    //getters
+    public String getName()
+    {
+        return name;
+    }
+    public ArrayList<Assignment> getAssignmentList()
+    {
+        return assignmentList;
+    }
+
+    //setters
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public void addAssignment(Assignment assignment1)
+    {
+        assignmentList.add(assignment1);
+    }
+
+
+}
